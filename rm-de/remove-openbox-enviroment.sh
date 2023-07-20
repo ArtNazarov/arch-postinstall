8fd617715eef464c84fe8a09100f3c51dac66a71
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2023-07-20 18:41:19 +0500

    Update remove-openbox-enviroment.sh

--- rm-de/remove-openbox-enviroment.sh
+++ rm-de/remove-openbox-enviroment.sh
@@ -3,23 +3,25 @@ echo "Remove OpenBox? [Y/N]?"
 echo "Confirm [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
-sudo pacman -Rsn obdevicemenu
-# sudo pacman -Rsn ttf-dejavu ttf-liberation
-sudo pacman -Rsn obconf
-#  sudo pacman -S roxterm
-sudo pacman -Rsn tint2
-#  sudo pacman -Rsn pavucontrol
-sudo pacman -Rsn vowheel
-sudo pacman -Rsn conky
-sudo pacman -Rsn picom
-yay -Rsn openbox-themes
-yay -Rsn obtheme
-yay -Rsn roxterm
-yay -Rsn obfilebrowser
-yay -Rsn obmenu-generator
-yay -Rsn volwheel
-yay -Rsn pa-applet-git
-sudo pacman -Rsn openbox
+
+        sudo pacman -Rsn obdevicemenu
+        # sudo pacman -Rsn ttf-dejavu ttf-liberation
+        sudo pacman -Rsn obconf
+        #  sudo pacman -S roxterm
+        sudo pacman -Rsn tint2
+        #  sudo pacman -Rsn pavucontrol
+        sudo pacman -Rsn vowheel
+        sudo pacman -Rsn conky
+        sudo pacman -Rsn picom
+        yay -Rsn openbox-themes
+        yay -Rsn obtheme
+        yay -Rsn roxterm
+        yay -Rsn obfilebrowser
+        yay -Rsn obmenu-generator
+        yay -Rsn volwheel
+        yay -Rsn pa-applet-git
+        sudo pacman -Rsn openbox
+        
 else
         echo "skipped OpenBox remove"
 fi
