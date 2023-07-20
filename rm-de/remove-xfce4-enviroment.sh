690cc78b8e60438c1cc989234bb81e83018b8f08
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2023-07-20 18:39:31 +0500

    Update remove-xfce4-enviroment.sh

--- rm-de/remove-xfce4-enviroment.sh
+++ rm-de/remove-xfce4-enviroment.sh
@@ -4,14 +4,14 @@ echo "Confirm [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
 
-sudo pacman -Rsn libxfce4ui
-sudo pacman -Rsn xfce4
-sudo pacman -Rsn xfwm4
-sudo pacman -Rsn xfce4-goodies
-sudo pacman -Rsn mousepad
-yay -Rsn menulibre
-sudo pacman -Rsn alacarte
-yay -Rsn xame
+        sudo pacman -Rsn libxfce4ui
+        sudo pacman -Rsn xfce4
+        sudo pacman -Rsn xfwm4
+        sudo pacman -Rsn xfce4-goodies
+        sudo pacman -Rsn mousepad
+        yay -Rsn menulibre
+        sudo pacman -Rsn alacarte
+        yay -Rsn xame
 
 else
         echo "skipped XFCE4 remove"
