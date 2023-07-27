95728c4a69a039fbb02517dd0549288681a8503b
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2023-07-27 18:52:33 +0500

    Update install-xfce4-enviroment.sh

--- de/install-xfce4-enviroment.sh
+++ de/install-xfce4-enviroment.sh
@@ -1,60 +1,60 @@
 cd ~
-echo "Remove xfce4? [Y/N]?"
+echo "Install xfce4? [Y/N]?"
 echo "Confirm [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
-        sudo pacman -Rcsn xfce4-panel-compiz
-        sudo pacman -Rcsn xfce4-settings
-        sudo pacman -Rcsn xfdesktop
-        sudo pacman -Rcsn xfce4-appfinder
-        sudo pacman -Rcsn xfce4-panel-compiz
-        sudo pacman -Rcsn xfce4-screensaver
-        sudo pacman -Rcsn xfce4-settings
-        sudo pacman -Rcsn xfce4-whiskermenu-plugin
-        sudo pacman -Rcsn xfdesktop
-        sudo pacman -Rcsn libxfce4ui
-        sudo pacman -Rcsn xfce4
-        sudo pacman -Rcsn xfwm4
-        sudo pacman -Rcsn xfce4-goodies
-        sudo pacman -Rcsn mousepad
-        sudo pacman -Rcsn menulibre
-        sudo pacman -Rcsn alacarte
-        sudo pacman -Rcsn xame
-        sudo pacman -Rcsn menulibre
-        sudo pacman -Rcsn exo
-        sudo pacman -Rcsn garcon
-        sudo pacman -Rcsn parole
-        sudo pacman -Rcsn ristretto
-        sudo pacman -Rcsn thunar
-        sudo pacman -Rcsn thunar-volman
-        sudo pacman -Rcsn xfce4-appfinder
-        sudo pacman -Rcsn  xfce4-notifyd
-        sudo pacman -Rcsn xfce4-panel-compiz
-        sudo pacman -Rcsn xfce4-power-manager
-        sudo pacman -Rcsn xfce4-session
-        sudo pacman -Rcsn xfce4-settings
-        sudo pacman -Rcsn xfce4-taskmanager
-        sudo pacman -Rcsn xfce4-terminal
-        sudo pacman -Rcsn xfdesktop
-        sudo pacman -Rcsn xfwm4
-        sudo pacman -Rcsn libxfce4ui
-        sudo pacman -Rcsn thunar-archive-plugin
-        sudo pacman -Rcsn equired by xfce4-battery-plugin
-        sudo pacman -Rcsn xfce4-datetime-plugin
-        sudo pacman -Rcsn xfce4-mount-plugin
-        sudo pacman -Rcsn xfce4-mpc-plugin
-        sudo pacman -Rcsn xfce4-notifyd
-        sudo pacman -Rcsn xfce4-pulseaudio-plugin
-        sudo pacman -Rcsn xfce4-screensaver
-        sudo pacman -Rcsn xfce4-screenshooter
-        sudo pacman -Rcsn xfce4-taskmanager
-        sudo pacman -Rcsn xfce4-wavelan-plugin
-        sudo pacman -Rcsn xfce4-whiskermenu-plugin
-        sudo pacman -Rcsn xfce4-panel
-        sudo pacman -Rcsn xfce4-whiskermenu-plugin
-        sudo pacman -Rcsn xfce4-xkb-plugin
+        sudo pacman -Scsn xfce4-panel-compiz
+        sudo pacman -Sy xfce4-settings
+        sudo pacman -Sy xfdesktop
+        sudo pacman -Sy xfce4-appfinder
+        sudo pacman -Sy xfce4-panel-compiz
+        sudo pacman -Sy xfce4-screensaver
+        sudo pacman -Sy xfce4-settings
+        sudo pacman -Sy xfce4-whiskermenu-plugin
+        sudo pacman -Sy xfdesktop
+        sudo pacman -Sy libxfce4ui
+        sudo pacman -Sy xfce4
+        sudo pacman -Sy xfwm4
+        sudo pacman -Sy xfce4-goodies
+        sudo pacman -Sy mousepad
+        sudo pacman -Sy menulibre
+        sudo pacman -Sy alacarte
+        sudo pacman -Sy xame
+        sudo pacman -Sy menulibre
+        sudo pacman -Sy exo
+        sudo pacman -Sy garcon
+        sudo pacman -Sy parole
+        sudo pacman -Sy ristretto
+        sudo pacman -Sy thunar
+        sudo pacman -Sy thunar-volman
+        sudo pacman -Sy xfce4-appfinder
+        sudo pacman -Sy  xfce4-notifyd
+        sudo pacman -Sy xfce4-panel-compiz
+        sudo pacman -Sy xfce4-power-manager
+        sudo pacman -Sy xfce4-session
+        sudo pacman -Sy xfce4-settings
+        sudo pacman -Sy xfce4-taskmanager
+        sudo pacman -Sy xfce4-terminal
+        sudo pacman -Sy xfdesktop
+        sudo pacman -Sy xfwm4
+        sudo pacman -Sy libxfce4ui
+        sudo pacman -Sy thunar-archive-plugin
+        sudo pacman -Sy xfce4-battery-plugin
+        sudo pacman -Sy xfce4-datetime-plugin
+        sudo pacman -Sy xfce4-mount-plugin
+        sudo pacman -Sy xfce4-mpc-plugin
+        sudo pacman -Sy xfce4-notifyd
+        sudo pacman -Sy xfce4-pulseaudio-plugin
+        sudo pacman -Sy xfce4-screensaver
+        sudo pacman -Sy xfce4-screenshooter
+        sudo pacman -Sy xfce4-taskmanager
+        sudo pacman -Sy xfce4-wavelan-plugin
+        sudo pacman -Sy xfce4-whiskermenu-plugin
+        sudo pacman -Sy xfce4-panel
+        sudo pacman -Sy xfce4-whiskermenu-plugin
+        sudo pacman -Sy xfce4-xkb-plugin
 else
-        echo "skipped XFCE4 remove"
+        echo "skipped XFCE4 install"
 fi
 
 cd -
