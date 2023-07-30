8c27e3ce56471629990ec19d63056c1a28cf1d94
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2023-07-30 10:02:17 +0500

    Update remove-cinnamon-enviroment.sh

--- rm-de/remove-cinnamon-enviroment.sh
+++ rm-de/remove-cinnamon-enviroment.sh
@@ -3,16 +3,17 @@ echo "Remove cinnamon? [Y/N]?"
 echo "Confirm [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
+        
+        sudo pacman -Rcsn cinnamon
+        yay -Rcsn xviewer
+        sudo pacman -Rcsn xreader
+        yay -Rcsn xplayer
+        yay -Rcsn pix
+        sudo pacman -Rcsn metacity
+        sudo pacman -Rcsn gnome-shell
+        yay -Rcsn mintlocale
+        yay -Rcsn cinnamon-translations
 
-sudo pacman -Rcsn cinnamon
-yay -Rcsn xviewer
-sudo pacman -Rcsn xreader
-yay -Rcsn xplayer
-yay -Rcsn pix
-sudo pacman -Rcsn metacity
-sudo pacman -Rcsn gnome-shell
-yay -Rcsn mintlocale
-yay -Rcsn cinnamon-translations
 else
         echo "skipped cinnamon remove"
 fi
